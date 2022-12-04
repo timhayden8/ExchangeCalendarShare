766e00bad37e3e5f46e11a4781bc8949b06aac89
timhayden8 <119264210+timhayden8@users.noreply.github.com>
2022-12-04 08:29:59 -0700

    Update Calendareditorrightsasssignmentexchangeonline.ps1

--- Calendareditorrightsasssignmentexchangeonline.ps1
+++ Calendareditorrightsasssignmentexchangeonline.ps1
@@ -1,15 +1,13 @@
 #connects to exchange online
 connect-exchangeonline
 #prompts user to enter calendar being shared. It then pulls an array of all users and everyone in the company gets editor rights
-$calendar= (Read-host "Enter calendar to share in this format email@email.com:\calendar")
-$users= (Get-mailbox -resultsize unlimited)
-foreach ($user in $users){
-    add-mailboxfolderpermission -identity $calendar -user $user.identity -accessrights editor
-}
-#Prompts user to enter email of user who needs permissions given. Then creates an array of mailbox calendars and applies editor privelege. 
 $User = Read-Host "Enter email address of person giving permissions to"
-$Mailboxes = Get-Mailbox -ResultSize Unlimited
+$Mailboxes= (Get-mailbox -resultsize unlimited)
+foreach ($mailbox in $mailboxes){
+    add-mailboxfolderpermission -identity $user":\Calendar" -user $mailbox.identity -accessrights editor
+}
+#This section shares the calendars of everyone in the organization with the original selected user as editor
 Foreach ($Mailbox in $Mailboxes) {
-    Add-MailboxFolderPermission -id $Mailbox":\Calendar" -user $user -AccessRights editor
+    Add-MailboxFolderPermission -identity $Mailbox":\Calendar" -user $user -AccessRights editor
 }
 Read-Host -prompt "Press Enter when Done"
