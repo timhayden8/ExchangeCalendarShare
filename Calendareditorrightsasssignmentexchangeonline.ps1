64d4447adbfc5b1ed9e1d37412e1440c5f6c24dc
timhayden8 <119264210+timhayden8@users.noreply.github.com>
2023-03-27 19:25:56 -0600

    Update Calendareditorrightsasssignmentexchangeonline.ps1

--- Calendareditorrightsasssignmentexchangeonline.ps1
+++ Calendareditorrightsasssignmentexchangeonline.ps1
@@ -1,13 +1,17 @@
-#connects to exchange online
-connect-exchangeonline
-#prompts user to enter calendar being shared. It then pulls an array of all users and everyone in the company gets editor rights
-$User = Read-Host "Enter email address of person giving permissions to"
-$Mailboxes= (Get-mailbox -resultsize unlimited)
-foreach ($mailbox in $mailboxes){
-    add-mailboxfolderpermission -identity $user":\Calendar" -user $mailbox.identity -accessrights editor
+#this function gives editor rights to a user's EO calendar and then gives the user editor rights on everyone in the tenant's calendar.
+#Example exchange-calendarshare -user tony@hawk.com
+function Exchange-CalendarShare
+{
+    param($user)
+    connect-exchangeonline
+    It then pulls an array of all users and everyone in the company gets editor rights
+    $Mailboxes= (Get-mailbox -resultsize unlimited)
+    foreach ($mailbox in $mailboxes){
+        add-mailboxfolderpermission -identity $user":\Calendar" -user $mailbox.identity -accessrights editor
+    }
+    #This section shares the calendars of everyone in the organization with the original selected user as editor
+    Foreach ($Mailbox in $Mailboxes) {
+        Add-MailboxFolderPermission -identity $Mailbox":\Calendar" -user $user -AccessRights editor
+    }
 }
-#This section shares the calendars of everyone in the organization with the original selected user as editor
-Foreach ($Mailbox in $Mailboxes) {
-    Add-MailboxFolderPermission -identity $Mailbox":\Calendar" -user $user -AccessRights editor
-}
-Read-Host -prompt "Press Enter when Done"
+Exchange-CalendarShare
